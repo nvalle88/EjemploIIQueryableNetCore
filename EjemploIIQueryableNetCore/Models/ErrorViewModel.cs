335f8b55df81ed9b50f89643e1b5a86c5bd698b6
Néstor Valle <nestorvalle880416@gmail.com>
2018-07-19 20:39:19 -0500

    Agregar archivos de proyecto.

---
+++ EjemploIIQueryableNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EjemploIIQueryableNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
